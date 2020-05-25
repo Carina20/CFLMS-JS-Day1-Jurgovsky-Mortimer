c8c73c41c7b1e4b8c6ef5b06469636b6f453a44e
Aki <kieran.mortimer@gmail.com>
2020-05-25 13:13:29 +0200

    added exercises 9-10

---
+++ Exercise 9/js/main.js
@@ -0,0 +1,16 @@
+let a = 7;
+let b = 14;
+let c = 21;
+let d = 36;
+let e = 42;
+
+let sum = a + b + c + d + e;
+
+let f = "1";
+let g = 15;
+let h = 8;
+let i = "1";
+
+let mulSum = f * g * h * i;
+
+document.write(sum / mulSum);
