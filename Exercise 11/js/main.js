0eecbdf0607e04c3c196bb032880812eb46dc1c0
Aki <kieran.mortimer@gmail.com>
2020-05-25 13:45:28 +0200

    challenge 1 complete

--- Exercise 11/js/main.js
+++ Exercise 11/js/main.js
@@ -1,1 +1,15 @@
-let wienArray = ["Vienna", "Berlin", "Newcastle", ""]
+let diagonalArray = [
+    ["Vienna", "Berlin", "Newcastle", "Pairs"],
+    ["was", "is", "will be", "could be"],
+    ["bad", "average", "nice", "super"],
+    ["town", "state", "village", "city"],
+];
+
+let outputString = "";
+
+for (i = 0; i <= 3; i++) {
+    outputString += diagonalArray[i][i];
+    outputString += " ";
+}
+
+document.write(outputString);
