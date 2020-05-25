628c7b00971b0c7d046e7fec86f7a3a97efdedb3
Aki <kieran.mortimer@gmail.com>
2020-05-25 12:02:44 +0200

    added exercises 6-8

---
+++ Exercise 6/js/main.js
@@ -0,0 +1,27 @@
+// Ex 6
+let fruit = ["apple", "banana", "kiwi"];
+
+document.write("Start = " + fruit);
+document.write("<br>");
+
+fruit.push("orange");
+document.write("Output = " + fruit);
+document.write("<br>");
+
+document.write("Output = " + fruit.slice(0, 3));
+
+document.write("<br>");
+document.write("<br>");
+
+// Ex 7
+let animal = ["monkey", "horse", "dog"];
+
+document.write("Start = " + animal);
+document.write("<br>");
+
+animal.reverse;
+document.write("Output = " + animal);
+document.write("<br>");
+
+animal.unshift("cat");
+document.write("Output = " + animal);
---
+++ Exercise 8/js/main.js
@@ -0,0 +1,18 @@
+let fruits = [
+    "mango",
+    "cherries",
+    "kiwi",
+    "grapes",
+    "pear",
+    "peach",
+    "orange",
+    "lemon",
+];
+
+document.write("Output:");
+document.write("<br>");
+
+for (let i of fruits) {
+    document.write(i);
+    document.write("<br>");
+}
